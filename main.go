2f54ead97c0b70adfb2851a8126ff8966cf54f71
Al-Ali Ibrahim <ibrahim@Al-Alis-MacBook-Air.local>
2023-05-17 14:18:06 +0100

    math

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-func calculateMedian(data []int) float64 {
+func calculateMedian(data []int) int {
 	// Sort the data in ascending order
 	sort.Ints(data)
 
@@ -19,33 +19,38 @@ func calculateMedian(data []int) float64 {
 		// If the length is even, average the middle two elements
 		middle1 := data[length/2-1]
 		middle2 := data[length/2]
-		return float64(middle1+middle2) / 2.0
+		return (middle1 + middle2) / 2
 	} else {
 		// If the length is odd, return the middle element
 		middle := data[length/2]
-		return float64(middle)
+		return middle
 	}
 }
-func calculateAverage(data []int) float64 {
+// The function calculates the average of a given slice of integers.
+func calculateAverage(data []int) int {
 	sum := 0
 	for _, value := range data {
 		sum += value
 	}
-	return float64(sum) / float64(len(data))
+	return sum / len(data)
 }
-func calculateVariance(data []int) float64 {
+// The function calculates the variance of a given set of integers.
+func calculateVariance(data []int) int {
 	average := calculateAverage(data)
-	sumOfSquares := 0.0
+	sumOfSquares := 0
 	for _, value := range data {
-		deviation := float64(value) - average
+		deviation := value - average
 		sumOfSquares += deviation * deviation
 	}
-	return sumOfSquares / float64(len(data))
+	return sumOfSquares / len(data)
 }
-func calculateStandardDeviation(data []int) float64 {
+// The function calculates the standard deviation of a given set of integer data.
+func calculateStandardDeviation(data []int) int {
 	variance := calculateVariance(data)
-	return math.Sqrt(variance)
+	return int(math.Round(math.Sqrt(float64(variance))))
 }
+// The function reads data from a file, calculates the median, average, variance, and standard
+// deviation of the data, and prints the results.
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("Please provide a file path as an argument.")
@@ -78,9 +83,8 @@ func main() {
 	average := calculateAverage(data)
 	variance := calculateVariance(data)
 	standardDeviation := calculateStandardDeviation(data)
-	fmt.Println("Median", median)
-	fmt.Println("Avarage", average)
-	fmt.Println("Variance", variance)
+	fmt.Println("Median:", median)
+	fmt.Println("Average:", average)
+	fmt.Println("Variance:", variance)
 	fmt.Println("Standard Deviation:", standardDeviation)
-
 }
